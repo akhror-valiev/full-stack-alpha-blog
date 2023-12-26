6ce971b2bb77ea73203aa5efdb176ee297aecef7
Oshie <oshie0115@gmail.com>
2023-12-26 06:30:30 +0500

    Add before save method to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+
+  before_save {self.email = email.downcase }
+
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 },

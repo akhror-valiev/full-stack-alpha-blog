eea12d057c02d1d002b9724707964d37f1bfa004
Oshie <oshie0115@gmail.com>
2024-01-16 09:39:24 +0500

    Added authorization to user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,8 +2,11 @@
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[show edit update destroy]
-  def show
-  end
+
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+
+  def show; end
 
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
@@ -13,12 +16,11 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:notice] = 'Article was created successfully!'
       redirect_to @article
@@ -50,4 +52,12 @@ class ArticlesController < ApplicationController
   def article_params
     params.require(:article).permit(:title, :description)
   end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "You can only edit or delete your own article"
+      redirect_to @article
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-
-  before_save {self.email = email.downcase }
-  has_many :article
+  before_save { self.email = email.downcase }
+  has_many :articles, dependent: :destroy
 
   validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 20 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -11,5 +10,4 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX }
 
   has_secure_password
-
 end
